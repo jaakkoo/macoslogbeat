cf085aac7c0bebe68f4e5564dbe495acce6c5081
Jaakko Aro <jaakko.aro@reaktor.fi>
2020-04-02 21:58:35 +0300

    Minor refactoring and a panic fix
    Starting to understand how things should be written with Go. Or at least
    I feel like I'm starting to understand it at least a little bit.
    
    Also fixes the panic close of closed channel.

--- beater/MacosLogBeat.go
+++ beater/MacosLogBeat.go
@@ -57,32 +57,35 @@ func buildArgs(command string, excluded []string) []string {
 func writeTimestamp(timestamp time.Time, fileName string) {
 	err := ioutil.WriteFile(fileName, []byte(timestamp.Format(rfc3339ms)), 0600)
 	if err != nil {
-		logp.Err("Could not update cache file timestamp")
+		logp.Err("Could not update cache file timestamp: %v", err)
 	}
 }
 
 func readTimestamp(fileName string) (time.Time, error) {
 	contents, err := ioutil.ReadFile(fileName)
 	if err != nil {
-		return time.Time{}, fmt.Errorf("Could not read contents of %s", fileName)
+		return time.Time{}, fmt.Errorf("Could not read contents of %s: %w", fileName, err)
 	}
 	t, err := time.Parse(rfc3339ms, string(contents))
 	if err != nil {
-		return time.Time{}, fmt.Errorf("Timestamp is malformed")
+		return time.Time{}, fmt.Errorf("Timestamp is malformed: %w", err)
 	}
 	return t, nil
 }
 
-func publishEvent(bt *macoslogbeat, eventFields *common.MapStr) {
-	ts, err := parseMacTimestamp((*eventFields)["timestamp"].(string))
+func publishEvent(bt *macoslogbeat, eventFields common.MapStr) {
+	ts, err := parseMacTimestamp(eventFields["timestamp"].(string))
 	if err != nil {
-		logp.Err("Cant use event timestamp, using time.Now().")
+		logp.Err("Cant use event timestamp, using time.Now(): %v", err)
 		ts = time.Now()
 	}
-	delete(*eventFields, "timestamp")
+	delete(eventFields, "timestamp")
+	if len(eventFields["eventMessage"].(string)) == 0 {
+		return
+	}
 	event := beat.Event{
 		Timestamp: ts,
-		Fields:    common.MapStr{"unifiedlog": *eventFields},
+		Fields:    common.MapStr{"unifiedlog": eventFields},
 	}
 	bt.client.Publish(event)
 }
@@ -91,32 +94,58 @@ func parseMacTimestamp(timestamp string) (time.Time, error) {
 	macTimestampFormat := "2006-01-02 15:04:05.000000Z0700"
 	t, err := time.Parse(macTimestampFormat, timestamp)
 	if err != nil {
-		return time.Time{}, fmt.Errorf("Timestamp conversion failed for '%s'", timestamp)
+		return time.Time{}, fmt.Errorf("Timestamp conversion failed for '%s': %w", timestamp, err)
 	}
 	return t, nil
 }
 
-func publishLogsSince(startTime time.Time, bt *macoslogbeat) error {
+func readLogWithArgs(commandArgs []string, skipLines int) (chan common.MapStr, error) {
+	var err error
+	cmd := exec.Command("/usr/bin/log", commandArgs...)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, fmt.Errorf("attaching pipe to commands stdout failed: %w", err)
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		return nil, fmt.Errorf("starting command failed: %w", err)
+	}
+
+	reader := bufio.NewReader(stdout)
+
+	if skipLines > 0 {
+		for i := 0; i < skipLines; i++ {
+			reader.ReadBytes('\n')
+		}
+	}
+
+	decoder := jstream.NewDecoder(reader, 1)
+	c := make(chan common.MapStr)
+	go func() {
+		for mv := range decoder.Stream() {
+			c <- common.MapStr(mv.Value.(map[string]interface{}))
+		}
+		close(c)
+		cmd.Wait()
+	}()
+	return c, nil
+}
+
+func publishLogsSince(bt *macoslogbeat, startTime time.Time) error {
+	var err error
 	layout := "2006-01-02 15:04:05Z0700"
 	args := append(buildArgs("show", bt.config.ExcludedSubsystems),
 		"--start", startTime.Format(layout),
 		"--end", time.Now().Format(layout))
-	cmd := exec.Command("/usr/bin/log", args...)
-	stdout, _ := cmd.StdoutPipe()
-	err := cmd.Start()
 
-	decoder := jstream.NewDecoder(bufio.NewReader(stdout), 1)
-	for mv := range decoder.Stream() {
-		f := common.MapStr(mv.Value.(map[string]interface{}))
-		// When viewing old logs creatorActivityID gets some really crazy values that are not indexable
-		f["creatorActivityID"] = float64(0)
-		publishEvent(bt, &f)
-	}
-
+	logStream, err := readLogWithArgs(args, 0)
 	if err != nil {
-		if exitError, ok := err.(*exec.ExitError); ok {
-			return fmt.Errorf("reading old logs did not exit succesfully exited with code %d", exitError.ExitCode())
-		}
+		return fmt.Errorf("could not old logs %w", err)
+	}
+	for fields := range logStream {
+		fields["creatorActivityID"] = float64(0)
+		publishEvent(bt, fields)
 	}
 
 	return nil
@@ -128,52 +157,32 @@ func (bt *macoslogbeat) Run(b *beat.Beat) error {
 	var err error
 	var counter int
 	timestampFile := "timestamp"
-	app := "/usr/bin/log"
-	cmd := exec.Command(app, buildArgs("stream", bt.config.ExcludedSubsystems)...)
-
-	stdout, err := cmd.StdoutPipe()
 	bt.client, err = b.Publisher.Connect()
 	if err != nil {
 		return err
 	}
 
-	ticker := time.NewTicker(bt.config.Period)
-
-	cmd.Start()
-	reader := bufio.NewReader(stdout)
-	if len(bt.config.ExcludedSubsystems) > 0 {
-		// Read the first line from buffer because it's not json when predicates are applied
-		reader.ReadBytes('\n')
-	}
-	dec := jstream.NewDecoder(reader, 1)
-
 	lastPublishedLog, err := readTimestamp(path.Join(bt.config.CacheDir, timestampFile))
 	if err == nil {
 		logp.Info("Shipping old logs since %s", lastPublishedLog.Format(rfc3339ms))
-		if err = publishLogsSince(lastPublishedLog, bt); err != nil {
+		if err = publishLogsSince(bt, lastPublishedLog); err != nil {
 			logp.Err(err.Error())
 		} else {
 			logp.Info("Old logs shipped")
 		}
 	}
 
-	for {
-		select {
-		case <-bt.done:
-			return nil
-		case <-ticker.C:
-		}
-
-		for mv := range dec.Stream() {
-			fields := common.MapStr(mv.Value.(map[string]interface{}))
-			eventTs, _ := parseMacTimestamp(fields["timestamp"].(string))
-			publishEvent(bt, &fields)
-			counter++
-			if counter%100 == 0 {
-				writeTimestamp(eventTs, path.Join(bt.config.CacheDir, timestampFile))
-			}
+	args := buildArgs("stream", bt.config.ExcludedSubsystems)
+	logStream, err := readLogWithArgs(args, 1)
+	for event := range logStream {
+		eventTs, _ := parseMacTimestamp(event["timestamp"].(string))
+		publishEvent(bt, event)
+		counter++
+		if counter%100 == 0 {
+			writeTimestamp(eventTs, path.Join(bt.config.CacheDir, timestampFile))
 		}
 	}
+	return nil
 }
 
 // Stop stops macoslogbeat.
